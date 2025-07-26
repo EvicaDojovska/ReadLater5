9391de095251d3a2c1a41477fbf2b87ae8995307
Evica Lazova <evica.dojovska@technoperia.com>
2025-07-26 18:12:46 +0200

    altering columns migration

---
+++ Data/Data/Migrations/20250726161146_renaming-column.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class renamingcolumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Categories",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "URL",
+                table: "Bookmarks",
+                newName: "Url");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Bookmarks",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Bookmarks",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ShortDescription",
+                table: "Bookmarks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Categories",
+                newName: "ID");
+
+            migrationBuilder.RenameColumn(
+                name: "Url",
+                table: "Bookmarks",
+                newName: "URL");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Bookmarks",
+                newName: "ID");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "URL",
+                table: "Bookmarks",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ShortDescription",
+                table: "Bookmarks",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
